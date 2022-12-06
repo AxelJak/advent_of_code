c151210c319c0da89e09cdf2b9aa27abae9912cf
Axel <axel.jakobsson3@gmail.com>
2022-12-06 18:20:21 +0100

    helper script

---
+++ helper_script.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+#Make a sub directory in the current directory with three files called Demo.txt, input.txt and Problem.md
+
+if len(sys.argv) == 1:
+    print("Please provide a directory name")
+    sys.exit(1)
+
+dir_name = sys.argv[1]
+
+if not os.path.exists("day" + dir_name):
+    os.mkdir("day" + dir_name)
+
+f = open("day" + dir_name + "/Demo.txt", "w")
+f.close()
+f = open("day" + dir_name + "/input.txt", "w")
+f.close()
+f = open("day" + dir_name + "/problem.md", "w")
+f.close()
