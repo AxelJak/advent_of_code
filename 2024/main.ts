5fe6bb1d85952754ef4ab985fa9b3f13a0c39f95
Axel Jakobsson <axel.jakobsson3@gmail.com>
2024-12-02 09:55:16 +0100

    day 1 part 2

--- 2024/main.ts
+++ 2024/main.ts
@@ -1,18 +1,24 @@
 const input = await Deno.readTextFile("./input.txt");
 
 const lines = input.split("\n");
-let list1;
-let list2;
-const list = lines.map(line => line.split("   ").map(Number));
+let list1 = []; 
+let list2 = [];
 
+const list = lines.map(line => line.split("   ").map(Number));
 list1 = list.map(line => line[0]);
 list2 = list.map(line => line[1]);
 
-list1 = list1.sort();
-list2 = list2.sort();
+let count = list2.reduce(function (value: any, value2: any) {
+  return (
+      value[value2] ? ++value[value2] :(value[value2] = 1),
+      value
+  );
+}, {});
 
 let sum = 0;
 for (let i = 0; i < list1.length; i++) {
-  sum += Math.abs(list1[i] - list2[i]);
+  if (count[list1[i]]) {
+    sum += list1[i] * count[list1[i]];
+  }
 }
 console.log(sum);
