7f267c6f8a3e223821b7af94ee29e9ac07cac3cd
Axel Jakobsson <axel.jakobsson3@gmail.com>
2024-12-02 11:51:05 +0100

    day 2 part 1 complete

--- 2024/main.ts
+++ 2024/main.ts
@@ -1,24 +1,58 @@
 const input = await Deno.readTextFile("./input.txt");
 
 const lines = input.split("\n");
-let list1 = []; 
-let list2 = [];
 
-const list = lines.map(line => line.split("   ").map(Number));
-list1 = list.map(line => line[0]);
-list2 = list.map(line => line[1]);
-
-let count = list2.reduce(function (value: any, value2: any) {
-  return (
-      value[value2] ? ++value[value2] :(value[value2] = 1),
-      value
-  );
-}, {});
+const rows = lines.map((line) => line.split(" ").map(Number));
 
 let sum = 0;
-for (let i = 0; i < list1.length; i++) {
-  if (count[list1[i]]) {
-    sum += list1[i] * count[list1[i]];
+rows.forEach((row) => {
+  if(isSafe(row)){
+    sum++;
   }
-}
+});
+
 console.log(sum);
+
+
+function isSafe(row: number[]):boolean{
+  const previous = row[0];
+  if(previous > row[1]){
+    return decreasing(row);
+  } else {
+    return increasing(row);
+  } 
+}
+
+function increasing(row: number[]):boolean{
+  let previous = row[0];
+  for(let i = 1; i < row.length; i++){
+    if (previous === row[i]){
+      return false;
+    }
+    if (previous > row[i]){
+      return false;
+    }
+    if (row[i] - previous > 3){
+      return false;
+    }
+    previous = row[i];
+  }
+  return true;
+}
+
+function decreasing(row: number[]):boolean{
+  let previous = row[0];
+  for(let i = 1; i < row.length; i++){
+    if (previous === row[i]){
+      return false;
+    }
+    if (previous < row[i]){
+      return false;
+    }
+    if (previous - row[i] > 3){
+      return false;
+    }
+    previous = row[i];
+  }
+  return true;
+}
